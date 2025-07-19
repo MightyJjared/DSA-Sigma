5364509afffd35c04be3b6bdd13a5c390bf5fd86
Md Abdullah <mohdabdullahansari345@gmail.com>
2025-07-19 19:43:20 +0530

    Added greedy codes

--- DSA/Heaps/map_library.cpp
+++ DSA/Heaps/map_library.cpp
@@ -3,12 +3,14 @@
 #include <string>
 using namespace std;
 
+// map consists of key value pair, where key is unique.
 int main(){
     map<int, string> m;
     m[100] = "Mohd";
-    m[101] = "Abdullah";
+    m[10] = "Abdullah";
     m[102] = "Ansari";
-
+  
+    // in the for loop maps will always print the smaller key value first
     for(auto iterator : m){
         cout << "Key = " << iterator.first << " , " << "Value = " << iterator.second << endl;
     }
