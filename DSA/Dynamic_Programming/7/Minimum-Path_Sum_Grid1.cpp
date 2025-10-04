dca5a9b65dec16924749df36dd703bda86964ca0
Md Abdullah <mohdabdullahansari345@gmail.com>
2025-10-05 03:27:04 +0530

    Added DP codes

---
+++ DSA/Dynamic_Programming/7/Minimum-Path_Sum_Grid1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <algorithm>
+using namespace std;
+
+// recursive
+// LeetCode 64: Minimum Path Sum
+// Explain: You need to go from top-left to bottom-right of a grid,
+// moving only right or down, with minimum sum of path.
+
+class Solution {
+public:
+
+    int helper(int m, int n, vector<vector<int>>& grid) {
+        if(m < 0 || n < 0){
+            return INT_MAX;
+            }
+        if(m ==0 && n== 0){
+            return grid[0][0];
+            }    
+    
+        int up = helper(m-1, n, grid);
+        int left = helper(m, n-1, grid);
+
+         if (up != INT_MAX){
+            up += grid[m][n];
+         } 
+        if (left != INT_MAX){
+            left += grid[m][n];
+        }
+
+        return min(up, left);
+}
+
+
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        return helper(m-1, n-1, grid);
+    }
+};
+
+int main() {
+    vector<vector<int>> grid = {
+        {1, 3, 1},
+        {1, 5, 1},
+        {4, 2, 1}
+    };
+
+    Solution sol;
+    cout << "Minimum Path Sum: " << sol.minPathSum(grid) << endl;
+    return 0;
+}
---
+++ DSA/Dynamic_Programming/7/Minimum_Path_Sum_Grid2.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+// memoization
+// LeetCode 64: Minimum Path Sum
+// Explain: You need to go from top-left to bottom-right of a grid,
+// moving only right or down, with minimum sum of path.
+
+class Solution {
+public:
+    int helper(int m, int n, vector<vector<int>>& grid, vector<vector<int>>& dp) {
+        if(m < 0 || n < 0){
+            return INT_MAX;
+        }
+        if(m ==0 && n== 0){
+            return grid[0][0];
+        }
+        if(dp[m][n] != -1){
+            return dp[m][n];
+        }
+
+        int up = helper(m-1,n,grid,dp);
+        int left = helper(m,n-1,grid,dp);
+
+        if (up != INT_MAX){
+            up += grid[m][n];
+         } 
+        if (left != INT_MAX){
+            left += grid[m][n];
+        }
+
+        dp[m][n] = min(up,left);
+        return dp[m][n];
+    }
+
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return helper(m-1, n-1, grid, dp);
+    }
+};
+
+int main() {
+    vector<vector<int>> grid = {
+        {1, 3, 1},
+        {1, 5, 1},
+        {4, 2, 1}
+    };
+
+    Solution sol;
+    cout << "Minimum Path Sum: " << sol.minPathSum(grid) << endl;
+    return 0;
+}
