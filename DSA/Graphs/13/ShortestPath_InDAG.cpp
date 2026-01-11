c8b0d6f11599dd891f1ef09f7163eedc4a80da3c
Md Abdullah <mohdabdullahansari345@gmail.com>
2026-01-11 06:48:22 +0530

    added graph codes

---
+++ DSA/Graphs/13/ShortestPath_InDAG.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <limits>
+using namespace std;
+
+/*
+G-27: Shortest Path in a Directed Acyclic Graph (DAG)
+
+You are given a directed acyclic graph with N nodes and weighted edges.
+The task is to compute the shortest distance from a given source node
+(usually node 0) to every other node in the graph.
+
+Typical approach:
+1. Perform a topological sort of the graph.
+2. Relax edges in topological order to update shortest distances.
+3. Nodes unreachable from source keep distance as INF.
+*/
+
+class Solution {
+public:
+    void toposort(int node, vector<vector<pair<int,int>>>& adj, vector<int> &visited, stack<int> &s){
+        visited[node] = true;
+
+        for(auto itr : adj[node]){
+            if(visited[itr.first] == false){
+                toposort(itr.first, adj, visited, s);
+            }
+        }
+        s.push(node);
+    }
+    vector<int> shortestPathDAG(int N, int M, vector<vector<pair<int,int>>>& adj, int src) {
+        // TODO: write your topological sort + shortest path logic here
+        vector<int> visited(N, 0);
+        stack<int> s;
+    
+        for(int i =0; i < N; i++){
+            if(visited[i] == false){
+                toposort(i,adj,visited, s);
+            }
+        }
+
+
+        vector<int> dist(N, 1e9); // placeholder, update in your logic
+        dist[src] = 0;
+
+        while(s.size() > 0){
+            int node = s.top();
+            s.pop();
+
+            for(auto itr : adj[node]){
+                int u = itr.first;
+                int wt = itr.second;
+
+                // edge relaxation
+                if(dist[node] + wt < dist[u]){
+                    dist[u] = dist[node] + wt;
+                }
+            }
+        }
+
+        return dist;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    int N = 6; // Example number of nodes
+    int M = 7; // number of edges
+
+
+    // adjacency list: adj[u] = list of {v, weight}
+    vector<vector<pair<int,int>>> adj(N);
+
+    // Example edges (u -> v with weight w)
+    adj[0].push_back({1, 2});
+    adj[0].push_back({4, 1});
+    adj[4].push_back({2, 2});
+    adj[1].push_back({2, 3});
+    adj[2].push_back({3, 6});
+    adj[4].push_back({5, 4});
+    adj[5].push_back({3, 1});
+
+    int src = 0;
+
+    vector<int> dist = sol.shortestPathDAG(N, M, adj, src);
+
+    cout << "Shortest distances from source " << src << ":\n";
+    for(int i = 0; i < N; i++){
+        cout << "Node " << i << ": " << dist[i] << "\n";
+    }
+
+    return 0;
+}
--- DSA/a.cpp
+++ DSA/a.cpp
@@ -3,74 +3,10 @@
 #include <vector>
 using namespace std;
 
-// printing
-// this is the non intiutive approach striver gave because through this we can easily print the sequence and also
-// it helps in solving different problems (striver dp 42)
-/*
-🔹 LeetCode Reference: 300. Longest Increasing Subsequence
-🔹 Problem Explanation:
-Given an integer array nums, return the length of the longest strictly increasing subsequence.
 
-Example:
-Input: nums = [10,9,2,5,3,7,101,18]
-Output: 4
-Explanation: The longest increasing subsequence is [2,3,7,101].
-*/
-
-class Solution {
-public:
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-
-        vector<int> dp(n, 1);
-
-        vector<int> hash(n);
-        for(int i =0; i < n; i++){
-            hash[i] = i;
-        }
-
-        int maximum = 1;
-        int index = 0;
-        for(int i =0; i <n; i++){
-            for(int prev = 0; prev < i; prev++){
-                if(nums[prev] < nums[i]){
-                    dp[i] = max(1 + dp[prev], dp[i];
-                    hash[i] = prev;
-                }
-            }
-            //maximum = max(maximum, dp[i]);
-            if(dp[i] > maximum){
-                maximum = dp[i];
-                index = i;
-            }
-        }
-
-        // We use a parent (hash) array to keep track of the previous index that contributed 
-        // to each element’s LIS value. After filling dp[], we backtrack using this hash array 
-        // to reconstruct and print the actual Longest Increasing Subsequence.
-
-        vector<int> ans;
-        ans.push_back(nums[index]);
-
-        while(hash[index] != index){
-            index = hash[index];
-            ans.push_back(nums[index]);
-        }
-        reverse(ans.begin(), ans.end());
-
-        cout << "Sequence = ";
-        for(auto itr : ans){
-            cout << itr << " ";
-        }
-        cout << endl;
-
-        return maximum;
-    }
-};
 
 int main() {
-    Solution sol;
-    vector<int> nums = {5, 4, 11, 1, 16, 8}; // Example input
-    cout << "Length = " << sol.lengthOfLIS(nums) << endl; // Expected output: 3
+    
+    cout << char(1 + 'a');
     return 0;
 }
